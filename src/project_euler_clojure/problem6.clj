21503e2bdd726e87edbbb7d3715d0095263e346f
Steven Merrill <steven.merrill@gmail.com>
2012-07-29 17:49:38 -0400

    Slight refactor of 6 to eliminate anon function.

--- src/project_euler_clojure/problem6.clj
+++ src/project_euler_clojure/problem6.clj
@@ -2,13 +2,16 @@
 
 (comment Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.)
 
+(defn square [x]
+  (* x x))
+
 (defn sum-of-squares [x]
   (apply +
-    (map #(* % %) (range 1 (+ 1 x)))))
+    (map square (range 1 (+ 1 x)))))
 
 (defn square-of-sum [x]
   (let [a (apply + (range 1 (+ 1 x)))]
-    (* a a)))
+    (square a)))
 
 (defn difference-sum-of-squares-and-square-of-sum [x]
   (- (square-of-sum x) (sum-of-squares x)))
