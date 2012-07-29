a150029c6e486eb5b8fcda8c175bf34ab401be75
Steven Merrill <steven.merrill@gmail.com>
2012-07-29 13:57:49 -0400

    Add skeleton project for Project Euler in Clojure.

---
+++ test/project_euler_clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns project-euler-clojure.test.core
+  (:use [project-euler-clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
