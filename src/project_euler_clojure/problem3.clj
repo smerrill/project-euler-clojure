5ca3925d0761a14eb89c9d12eb4d2fb466d95e45
Steven Merrill <steven.merrill@gmail.com>
2012-07-29 17:05:30 -0400

    Working problem 3!
    This has lots of failed attempts commented out.

--- src/project_euler_clojure/problem3.clj
+++ src/project_euler_clojure/problem3.clj
@@ -2,15 +2,58 @@
 
 (comment What is the largest prime factor of the number 600851475143 .)
 
+;(defn divisors [n]
+  ;(filter #(= 0 (mod n %)) (range 1 (int (Math/sqrt n)))))
+
 (defn divisors [n]
-  (filter #(= 0 (mod n %)) (range 1 (+ 1 n))))
+  (sort
+    (mapcat
+      #(list % (/ n %))
+      (for [a (range 1 (+ 1(int (Math/sqrt n))))
+        :when (= 0 (mod n a))]
+      a))))
+
+;(defn lazy-divisors [n]
+    ;(for [a (range n 0 -1)
+      ;:when (= 0 (mod n a))]
+    ;a))
 
 (defn is-prime? [n]
-  (or
-    (= n 1)
+  (and
+    (> n 1)
     (= (divisors n) (list 1 n))))
 
-(defn prime-factors [n]
-  (filter is-prime? (divisors n)))
+;(defn lazy-prime-factors [n]
+  ;(for [a (lazy-divisors n)
+    ;:let [b (int (Math/sqrt n))]
+    ;:when (and (< a b) (is-prime? a))]
+    ;a))
 
-(last (prime-factors 600851475143))
+;(defn lazy-prime-factors [n]
+  ;(for [a (lazy-divisors n)
+    ;:let [b (int (Math/sqrt n))]
+    ;:when (and (< a b) (is-prime? a))]
+    ;a))
+
+;divisors(defn prime-factors [n]
+  ;(filter is-prime? (divisors n)))
+
+;(defn largest-prime-factor [n]
+  ;(first
+    ;(for [a (range n 0 -1)
+      ;:when (is-prime? divisors a)]
+    ;a)))
+
+(last (filter is-prime? (divisors 600851475143)))
+;(divisors 600851475143)
+
+;(second (lazy-divisors 13195))
+;(first (lazy-prime-factors 13195))
+;(lazy-prime-factors 13195)
+
+;(last (prime-factors 13195))
+
+;(last (prime-factors 600851475143))
+
+;(is-prime? 27)
+;(is-prime? 17)
